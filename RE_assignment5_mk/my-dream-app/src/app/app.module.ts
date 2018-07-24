1ab7f61c11e70d3626e5dcba07723cdaf2a89535
mason.y.koh <ko.terra.119@gmail.com>
2018-07-24 17:06:57 -0400

    cmd executed: ng g component (home, employees, positions) --flat

--- RE_assignment5_mk/my-dream-app/src/app/app.module.ts
+++ RE_assignment5_mk/my-dream-app/src/app/app.module.ts
@@ -6,13 +6,19 @@ import { AppComponent } from './app.component';
 import { NavComponent } from './nav.component';
 import { ContentComponent } from './content.component';
 import { FooterComponent } from './footer.component';
+import { HomeComponent } from './home.component';
+import { EmployeesComponent } from './employees.component';
+import { PositionsComponent } from './positions.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
     ContentComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    EmployeesComponent,
+    PositionsComponent
   ],
   imports: [
     BrowserModule,
---
+++ RE_assignment5_mk/my-dream-app/src/app/positions.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-positions',
+  template: `
+    <p>
+      positions works!
+    </p>
+  `,
+  styles: []
+})
+export class PositionsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
