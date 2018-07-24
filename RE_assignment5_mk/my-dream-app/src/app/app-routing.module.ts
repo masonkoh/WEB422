1df791d3c1a6db3f01ba721dbf756f841b543a19
mason.y.koh <ko.terra.119@gmail.com>
2018-07-24 19:28:58 -0400

    under construction: app-routing.module.ts
    - if comment out like before, it works(original code is commented out above)

--- RE_assignment5_mk/my-dream-app/src/app/app-routing.module.ts
+++ RE_assignment5_mk/my-dream-app/src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
+// import { NgModule } from '@angular/core';
+// import { Routes, RouterModule } from '@angular/router';
+
+// const routes: Routes = [];
+
+// @NgModule({
+//   imports: [RouterModule.forRoot(routes)],
+//   exports: [RouterModule]
+// })
+// export class AppRoutingModule { }
+
+
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home.component';
+import { EmployeesComponent } from './employees.component';
+import { PositionsComponent } from './positions.component';
+import { PageNotFoundComponent } from './page-not-found.component';
+const routes: Routes = [
+
+  { path: 'home', component: HomeComponent },
+  { path: 'employees', component: EmployeesComponent},
+  { path: 'positions', component: PositionsComponent},
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
