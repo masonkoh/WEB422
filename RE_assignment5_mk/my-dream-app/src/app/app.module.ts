183601db91484afd90c2876f99c1e5ab913b25d5
mason.y.koh <ko.terra.119@gmail.com>
2018-07-24 16:33:59 -0400

    cmd executed: (ng g component Nav --flat) (ng g component Content --flat) (ng g component Footer --flat)

--- RE_assignment5_mk/my-dream-app/src/app/app.module.ts
+++ RE_assignment5_mk/my-dream-app/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav.component';
+import { ContentComponent } from './content.component';
+import { FooterComponent } from './footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    ContentComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
---
+++ RE_assignment5_mk/my-dream-app/src/app/content.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-content',
+  template: `
+    <p>
+      content works!
+    </p>
+  `,
+  styles: []
+})
+export class ContentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
