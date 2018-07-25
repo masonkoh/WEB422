3e8abfd3e2f18fcfdaf6acce5dd8e2d511c24343
mason.y.koh <ko.terra.119@gmail.com>
2018-07-24 22:48:56 -0400

    problem solved: Observable thing from 'position.service.ts'
     - cmd executed: npm install rxjs-compat --save

--- RE_assignment5_mk/my-dream-app/src/app/position.service.ts
+++ RE_assignment5_mk/my-dream-app/src/app/position.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http"
 import { Position } from "./data/position";
-import { Observable } from "Observable";
+import { Observable } from "rxjs/Observable";
 
 @Injectable()
 export class PositionService {
