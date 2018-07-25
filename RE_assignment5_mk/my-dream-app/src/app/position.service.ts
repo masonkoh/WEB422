2a0c616f430209377d65abc27dbbfd0957e855cc
mason.y.koh <ko.terra.119@gmail.com>
2018-07-24 22:46:25 -0400

    heroku links edited

--- RE_assignment5_mk/my-dream-app/src/app/position.service.ts
+++ RE_assignment5_mk/my-dream-app/src/app/position.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http"
+import { Position } from "./data/position";
+import { Observable } from "Observable";
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class PositionService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  
+  getPositions() : Observable<Position[]>{
+    return this.http.get<Position[]>("https://stormy-depths-28130.herokuapp.com/positions");
+  }
+
+  getPosition(id : string) : Observable<Position[]>{
+   return this.http.get<Position[]>("https://stormy-depths-28130.herokuapp.com/position/"+id);
+  }
+  
+  savePosition(position : Position){
+    return this.http.put<any>("https://stormy-depths-28130.herokuapp.com/position/"+position._id, position);
+  }
 }
