6d50544f16c1ddda6710c776033181ef3130a529
mason.y.koh <ko.terra.119@gmail.com>
2018-07-24 20:42:37 -0400

    IMPORTANT MSG: problem almost solved!
    - Now I can road homepage with <router-outlet> and import things @content.component.html / app-routing.module.ts
     - the problem was PageNotFoundComponent. if I comment them out from (app-routing.module.ts), then i can see the proper home

--- RE_assignment5_mk/my-dream-app/src/app/app-routing.module.ts
+++ RE_assignment5_mk/my-dream-app/src/app/app-routing.module.ts
@@ -16,14 +16,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home.component';
 import { EmployeesComponent } from './employees.component';
 import { PositionsComponent } from './positions.component';
-import { PageNotFoundComponent } from './page-not-found.component';
+// import { PageNotFoundComponent } from './page-not-found.component';
 const routes: Routes = [
 
   { path: 'home', component: HomeComponent },
   { path: 'employees', component: EmployeesComponent},
   { path: 'positions', component: PositionsComponent},
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: '**', component: PageNotFoundComponent }
+  // { path: '**', component: PageNotFoundComponent }
 ];
 
 @NgModule({
