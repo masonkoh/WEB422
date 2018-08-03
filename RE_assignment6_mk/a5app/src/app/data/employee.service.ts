829f42ae602d85758d38cc5ef42af56184801626
mason.y.koh <ko.terra.119@gmail.com>
2018-08-03 06:10:38 -0400

    saveEmployee/getEmployee added in 'employee.service.ts'

--- RE_assignment6_mk/a5app/src/app/data/employee.service.ts
+++ RE_assignment6_mk/a5app/src/app/data/employee.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Employee } from './employee';
 import { HttpClient } from "@angular/common/http";
 import { Observable } from "rxjs/Observable";
+import { EmployeeRaw } from "./employeeRaw";
 
 @Injectable()
 export class EmployeeService {
@@ -10,6 +11,15 @@ export class EmployeeService {
 
     getEmployees(): Observable<Employee[]> {
         return this.http.get<Employee[]>("https://radiant-mesa-48727.herokuapp.com/employees");
+    }
+
+    saveEmployee(employee: EmployeeRaw): Observable<any> {
+        return this.http.put<any>("https://radiant-mesa-48727.herokuapp.com/employee/" + employee._id, employee);
+    }
+
+    getEmployee(id: number): Observable<EmployeeRaw[]> {
+        return this.http.get<EmployeeRaw[]>("https://radiant-mesa-48727.herokuapp.com/employee-raw/" + id);
+
     }
 
 }
