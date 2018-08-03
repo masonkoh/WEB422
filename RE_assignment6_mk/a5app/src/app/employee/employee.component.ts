360cd4b8fa48d3ab8ad7d29a5b7cdc6ec2e5f098
mason.y.koh <ko.terra.119@gmail.com>
2018-08-03 07:10:34 -0400

    Methods: - constructor() In this method, we must inject the following Services: - EmployeeService from employee.service - ActivatedRoute from @angular/router - PositionService from position.service

--- RE_assignment6_mk/a5app/src/app/employee/employee.component.ts
+++ RE_assignment6_mk/a5app/src/app/employee/employee.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { EmployeeRaw } from '../data/EmployeeRaw'; // THIS MIGHT COULD CAUSE ERROR './data/EmployeeRaw' <-- is this better? 
+import { EmployeeService } from '../data/employee.service';
+import { PositionService } from '../data/position.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-employee',
@@ -6,6 +10,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./employee.component.css']
 })
 export class EmployeeComponent implements OnInit {
+
+  private paramSubScription: any;
+  private employeeSubscription: any;
+  private getPositionsSubcription: any;
+  private saveEmployeeSubscription: any;
+  private employee: EmployeeRaw;
+  private positions: Position[];
+  private successMessage = false;
+  private failMessage = false;
+
 
   constructor() { }
 
